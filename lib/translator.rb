f405bc1ab72c254cbf305ffe6000330465097910
James P Bondeson <jpbond58@gmail.com>
2020-08-18 18:29:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,19 +4,22 @@ require "pry"
 def load_library(emoticon_files)
   # code goes here
   emoticons = YAML.load_file('./lib/emoticons.yml')
-    new_hash = {}
-    emoticons.each do |key,val|
-            new_hash[key] = {}
-            new_hash[key][:english]= val[0]
-            new_hash[key][:japanese]=val[1]  `                         `
-    end
-  new_hash
+  new_hash = {}
+   emoticons.each do |key,val|
+     new_hash[key] = {}
+     new_hash[key][:english]= val[0]
+     new_hash[key][:japanese]=val[1]
+     
+#  binding.pry
+   end
+new_hash
+#binding.pry
 end
 
-
-def get_english_meaning
+def get_japanese_emoticon
   # code goes here
 end
-def get_japanese_emoticon
+
+def get_english_meaning
   # code goes here
 end
