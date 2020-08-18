4023228ccd5c2413a62144ecf2fd8c37e295a922
James P Bondeson <jpbond58@gmail.com>
2020-08-18 19:50:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ end
 
 def get_english_meaning(emoticon_files, emoticons)
   english_meaning = {}
-  load_library(emoticon_files).each_with_object {|key|}
+  load_library(emoticon_files).each_with_object({}) do |key, val|
   english_meaning = emoticons
 
   end
