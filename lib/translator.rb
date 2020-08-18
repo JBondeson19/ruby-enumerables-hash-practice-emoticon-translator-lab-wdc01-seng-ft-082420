ecc45f5d03a1b1ba06804ad5391b189756ae12b7
James P Bondeson <jpbond58@gmail.com>
2020-08-18 16:57:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(emoticon_files)
   # code goes here
   emoticons = YAML.load_file('./lib/emoticons.yml')
 binding.pry
-  Hash[emotes.map{|(key,val)| [key.to_sym,val]}]
+  Hash[emoticons.map{|(key,val)| [key.to_sym,val]}]
   emotes = { 'meaning' => {},
               'values' => {},
 
