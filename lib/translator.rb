dbbaa40757fdc299eb8d956854847cfda65b1b73
James P Bondeson <jpbond58@gmail.com>
2020-08-18 18:30:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,10 +12,12 @@ def load_library(emoticon_files)
 new_hash
 end
 
+def get_english_meaning
+  # code goes here
+end
+
 def get_japanese_emoticon
   # code goes here
 end
 
-def get_english_meaning
-  # code goes here
-end
+
